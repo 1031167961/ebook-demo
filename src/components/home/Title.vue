02b071e3aaf1157d1bfde1f8016885c5bada1983
1031167961@qq.com <panyitao@huatu.com>
2021-08-05 14:05:30 +0800

    书城首页组件集成

---
+++ src/components/home/Title.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="home-title-wrapper">
+    <div class="label">{{label}}</div>
+    <div class="btn" @click="click">{{btn}}</div>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+    props: {
+      label: String,
+      btn: String
+    },
+    methods: {
+      click() {
+        this.$emit('onClick')
+      }
+    }
+  }
+</script>
+
+<style lang="scss" rel="stylesheet/scss" scoped>
+  @import "../../assets/styles/global";
+  .home-title-wrapper {
+    width: 100%;
+    padding: pr(15) pr(10);
+    box-sizing: border-box;
+    @include top;
+    .label {
+      flex: 1;
+      font-size: pr(18);
+      color: #333;
+      font-weight: bold;
+    }
+    .btn {
+      font-size: pr(14);
+      color: rgba(64, 158, 255, 1);
+      &:active {
+        color: rgba(64, 158, 255, .5);
+      }
+    }
+  }
+</style>
--- src/views/store/StoreHome.vue
+++ src/views/store/StoreHome.vue
@@ -3,35 +3,16 @@
    <search-bar></search-bar>
    <flap-card :data="random"></flap-card>
    <scroll :top="scrollTop" @onScroll="onScroll" ref="scroll">
-     <div>111111</div>
-     <div>111111</div>
-     <div>111111</div>
-     <div>111111</div>
-     <div>111111</div>
-     <div>111111</div>
-     <div>111111</div>
-     <div>111111</div>
-     <div>111111</div>
-     <div>111111</div>
-     <div>111111</div>
-     <div>111111</div>
-     <div>111111</div>
-     <div>111111</div>
-     <div>111111</div>
-     <div>111111</div>
-     <div>111111</div>
-     <div>111111</div>
-     <div>111111</div>
-     <div>111111</div>
-     <div>111111</div>
-     <div>111111</div>
-     <div>111111</div>
-     <div>111111</div>
-     <div>111111</div>
-     <div>111111</div>
-     <div>111111</div>
-     <div>111111</div>
-     <div>111111</div>
+     <div class="banner-wrapper">
+       <div class="banner-img" :style="{backgroundImage: `url('${banner}')`}"></div>
+     </div>
+     <guess-you-like :data="guessYouLike"></guess-you-like>
+     <recommend :data="recommend" class="recommend"></recommend>
+     <featured :data="featured" class="featured" :titleText="$t('home.featured')" :btnText="$t('home.seeAll')"></featured>
+     <div class="category-list-wrapper" v-for="(item, index) in categoryList" :key="index">
+       <category-book :data="item"></category-book>
+     </div>
+     <category :data="categories" class="categories"></category>
    </scroll>
  </div>
 </template>
@@ -42,18 +23,34 @@ import FlapCard from '../../components/home/FlapCard'
 import Scroll from '../../components/common/Scroll'
 import { storeHomeMixin } from '../../utils/mixin'
 import { home } from '../../api/store'
+import GuessYouLike from '../../components/home/GuessYouLike'
+import Recommend from '../../components/home/Recommend'
+import Featured from '../../components/home/Featured'
+import CategoryBook from '../../components/home/CategoryBook'
+import Category from '../../components/home/Category'
 
 export default {
   mixins: [storeHomeMixin],
   components: {
     SearchBar,
     Scroll,
-    FlapCard
+    FlapCard,
+    GuessYouLike,
+    Recommend,
+    Featured,
+    CategoryBook,
+    Category
   },
   data() {
     return {
       scrollTop: 94,
-      random: null
+      random: null,
+      banner: null,
+      guessYouLike: null,
+      recommend: null,
+      featured: null,
+      categoryList: [],
+      categories: []
     }
   },
   methods: {
@@ -71,8 +68,15 @@ export default {
     home().then(response => {
       if(response && response.status === 200) {
         const data = response.data
+        console.log(data)
         const randomIndex = Math.floor(Math.random() * data.random.length)
         this.random = data.random[randomIndex]
+        this.banner = data.banner
+        this.guessYouLike = data.guessYouLike
+        this.recommend = data.recommend
+        this.featured = data.featured
+        this.categoryList = data.categoryList
+        this.categories = data.categories
       }
     })
   }
@@ -84,5 +88,28 @@ export default {
 .store-home {
   width: 100%;
   height: 100%;
+  .banner-wrapper {
+    width: 100%;
+    padding: pr(10);
+    box-sizing: border-box;
+    .banner-img {
+      width: 100%;
+      height: pr(150);
+      background-repeat: no-repeat;
+      background-size: 100% 100%;
+    }
+  }
+  .recommend {
+    margin-top: pr(20);
+  }
+  .featured {
+    margin-top: pr(20);
+  }
+  .category-list-wrapper {
+    margin-top: pr(20);
+  }
+  .categories {
+    margin-top: pr(20);
+  }
 }
 </style>
